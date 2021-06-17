e434383b4d22d4014ebb017c2568285a8096b17a
Christian Rodas <54185006+ChristianMRodas@users.noreply.github.com>
2021-06-17 12:13:30 -0500

    Added Source Files
    Contains the code that was for the logic and mechanics of the games

---
+++ GameManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;  //YUses for UI
+using UnityEngine.SceneManagement; //used to restart scene
+using UnityEngine.UI; //lets us interact with the restart button (or any button
+
+public class GameManager : MonoBehaviour
+{
+    //we can pass the datatype (whereas in arrays u have to say what it is
+    public List<GameObject> targets;
+
+    public TextMeshProUGUI scoreText;  //For UI //***Dont forget to add it to the GamerManager scriptin Unity
+
+    public TextMeshProUGUI gameOverText; //In Unity, turn off the text (checkbox next to name), n we will
+    //  turn back on via code
+
+    public Button restartButton; //Note: add to the GM script in Unity
+
+    public GameObject titleScreen; //Note: add to the GM script in untiy
+
+    public bool isGameActive;
+
+    private int score;
+
+    private float spawnRate = 1.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive) //if its true, run it
+        {
+            yield return new WaitForSeconds(spawnRate); //spawnRate is spawn interval rate
+            int index = Random.Range(0, targets.Count);  //which object to spawn
+
+            Instantiate(targets[index]); //create object
+
+        }
+ 
+    }
+
+    //used to calc score
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+
+        gameOverText.gameObject.SetActive(true); //turns on the text //need to set ref in Unity (in GM script)
+
+        isGameActive = false;
+    }
+
+    //*Note: In Unity, go to button, on click(), add this GM script (drag n drop), function will be RestartGame()
+    public void RestartGame() //Uses the unisng Unity.NEgine.scenemanagement ...
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //u can also just pass Prototype 5
+
+        
+
+    }
+
+    public void StartGame(int difficulty)
+    {
+
+        isGameActive = true; //should be on top, before the Co-routine
+        score = 0; //Need to start it with a number
+
+        spawnRate /= difficulty; //higher diff, faster spawn rate
+
+        StartCoroutine(SpawnTarget());
+
+        UpdateScore(0); //resets score
+
+        titleScreen.gameObject.SetActive(false);
+    }
+}
---
+++ Target.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private GameManager gameManager; //so that it can use the GM script on here as well (like scores on destory
+    //      *then in start, add the .Find + GetComps...
+
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+
+    private float xRange = 4;
+    private float ySpawnPos = -2;
+
+    public int pointValue; //go to unity to udpate values (pionts per obj
+
+    public ParticleSystem explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        //<GM> not G..Obj..
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>(); //Finds the script, gets its components
+        //      so that we can use them here
+
+
+        //Random x, random height(y), random rotation
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        
+        transform.position = RandomSpawnPos(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //overrides a unity premade mehtod
+    //used to destory objects just by clicking down on the mosue
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+
+            //Pos is where the current game obj is at,  sets particle rot's as the same as before
+            //  *Note update on Unity
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+
+            //updatres score (*note: use the period  //Note: method needs to be public (else cant access it)
+            //5 increments
+
+            gameManager.UpdateScore(pointValue); //comes form the GM script //Passes the point values assigned
+                                                 //  from Unity, and updates correct score
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject); //Des obj when falls below map
+
+        //created a tag "Bad" for the skull item, n assigned it to it
+        // so now if the gameObj IS NOT the "bad" obj, its game over (so good item falls below the map, game over)
+        if(!gameObject.CompareTag("Bad"))  
+        {
+            gameManager.GameOver(); //displays game over
+        }
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        //uses the "new" keyword bcuz we are actually creating a new Vector3 (whereas RandomForce just multiplies it
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
